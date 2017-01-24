45dd8a462c717d4f11f2db302020e3c85b7a4236
Tom Fogal <tfogal@sci.utah.edu>
2017-01-23 21:19:09 -0800

    Initial stab at representing variable relations.
    i.e. the ability to encode that a variable parameter from one
    method must be sourced from the parameter of a different method.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::btree_map::BTreeMap;
 use std::fmt::Write;
 use std::fs::File;
 use std::process::Command;
@@ -5,16 +6,18 @@ extern crate rand;
 mod bitvector;
 mod bloom;
 
-#[derive(PartialEq)]
+#[derive(Clone, PartialEq)]
 #[allow(dead_code)]
 enum Type {
 	U8, I8, U16, I16, U32, I32, U64, I64, F32, F64,
-	USIZE, INTEGER, UNSIGNED,
-	UDT(String, Box<Type>),
+	Usize, Integer, Unsigned,
+	Character,
+	Void,
+	Enum(String, BTreeMap<String, u32>), // name + set of values
+	// Each Type in the vec is assumed to be a Field.
+	UDT(String, Vec<Box<Type>>),
+	Field(String, Box<Type>),
 	Pointer(Box<Type>),
-	/* Nil is a sentinel to end recursion, specifically for UDTs.  For example,
-	 * "struct ty;" is an opaque type, so its typelist is just 'Nil'. */
-	Nil,
 }
 trait Name {
 	fn name(&self) -> String;
@@ -25,7 +28,7 @@ macro_rules! tryp {
 }
 
 impl Name for Type {
-	fn name(&self) -> String {
+	fn name<'a>(&'a self) -> String {
 		use Type::*;
 		let mut res = String::new();
 		match self {
@@ -35,20 +38,33 @@ impl Name for Type {
 			&U32 => "uint32_t", &I32 => "int32_t",
 			&U64 => "uint64_t", &I64 => "int64_t",
 			&F32 => "float", &F64 => "double",
-			&USIZE => "size_t", &INTEGER => "int", &UNSIGNED => "unsigned",
+			&Usize => "size_t", &Integer => "int", &Unsigned => "unsigned",
+			&Character => "char",
+			&Void => "void",
+			&Enum(ref enm, _) => {
+				tryp!(write!(&mut res, "enum {}", enm));
+				res.as_str().clone()
+			},
 			&Type::UDT(ref udt, _) => udt,
+			&Type::Field(_, ref ty) => {
+				tryp!(write!(&mut res, "{}", ty.name()));
+				res.as_str().clone()
+			},
 			&Type::Pointer(ref t) => {
 				tryp!(write!(&mut res, "{}*", t.name()));
 				res.as_str().clone()
 			},
-			&Nil => "bug; type is nil!"
 		}.to_string()
 	}
 }
 
+type Arguments = Vec<Type>;
+type ReturnType = Type;
+
+#[derive(Clone)]
 struct Function {
 	return_type: Type,
-	arguments: Vec<Type>,
+	arguments: Arguments,
 	name: String,
 }
 struct ValueU64 {
@@ -67,15 +83,57 @@ impl ValueU64 {
 	pub fn get(&mut self) -> u64 {
 		use rand::distributions::IndependentSample;
 		let mut sample = self.distr.ind_sample(&mut self.rng);
+		let mut i: usize = 0;
 		while self.tested.query(sample) {
 			sample = self.distr.ind_sample(&mut self.rng);
+			// 'complete' is expensive so we don't check every time.
+			i = i+1;
+			if i >= 512 && self.tested.complete() {
+				break;
+			}
 		}
 		self.tested.add(sample);
 		return sample;
 	}
 }
 
-fn prototypes(strm: &mut std::io::Write, functions: &Vec<Function>) {
+enum VariableSource<'a> {
+	Free,
+	Parameter(&'a Function, usize), // function + parameter index it comes from
+	ReturnValue(&'a Function),
+}
+
+enum VariableUse<'a> {
+	Nil, // isn't used.
+	Argument(&'a Function, usize), // function + parameter index it comes from
+}
+
+// A dependent variable is a variable that we don't actually have control over.
+// For example, if the API model states that 'the return value of f() must be
+// the second argument of g()', a la:
+//   type v = f();
+//   g(_, v);
+// Then 'v' is dependent.  The effect is mostly that we don't attach a Value to
+// it.
+#[allow(dead_code)]
+struct DependentVariable<'a> {
+	name: String,
+	src: VariableSource<'a>,
+	dest: VariableUse<'a>,
+	// do we need a type: Type ?
+}
+
+// A free variable is a variable that we DO have control over.  This generally
+// means variables that are API inputs.
+#[allow(dead_code)]
+struct FreeVariable<'a> {
+	name: String,
+	tested: ValueU64, // probably want to parametrize
+	dest: VariableUse<'a>,
+	ty: &'a Type,
+}
+
+fn prototypes(strm: &mut std::io::Write, functions: &Vec<&Function>) {
 	for fqn in functions.iter() {
 		tryp!(write!(strm, "extern {} {}(", fqn.return_type.name(), fqn.name));
 		for a in 0..fqn.arguments.len() {
@@ -88,7 +146,7 @@ fn prototypes(strm: &mut std::io::Write, functions: &Vec<Function>) {
 	}
 }
 
-fn generate(mut strm: &mut std::io::Write, functions: &Vec<Function>,
+fn generate(mut strm: &mut std::io::Write, functions: &Vec<&Function>,
             v64: &mut ValueU64) {
 	tryp!(writeln!(strm, "#include <stdio.h>"));
 	tryp!(writeln!(strm, "#include <stdlib.h>"));
@@ -134,13 +192,62 @@ fn generate(mut strm: &mut std::io::Write, functions: &Vec<Function>,
 	tryp!(writeln!(strm, "}}"));
 }
 
+
+
 fn main() {
-	let hs_data = Type::UDT("struct hsearch_data".to_string(), Box::new(Type::Nil));
-	let hs_data_ptr: Type = Type::Pointer(Box::new(hs_data));
-	let hcreate_r_args = vec![Type::USIZE, hs_data_ptr];
-	let hcreate_r  = Function { return_type: Type::INTEGER,
+	let hs_data = Type::UDT("struct hsearch_data".to_string(), vec![]);
+	let hs_data_ptr: Type = Type::Pointer(Box::new(hs_data.clone()));
+	let hcreate_r_args = vec![Type::Usize, hs_data_ptr];
+	let hcreate_r  = Function { return_type: Type::Integer,
 	                            arguments: hcreate_r_args,
 	                            name: "hcreate_r".to_string() };
+	let entry = Type::UDT("ENTRY".to_string(),
+		vec![Box::new(Type::Pointer(Box::new(Type::Character))),
+		     Box::new(Type::Pointer(Box::new(Type::Void)))]
+	);
+	let mut action_values: BTreeMap<String, u32> = BTreeMap::new();
+	action_values.insert("FIND".to_string(), 0);
+	action_values.insert("ENTER".to_string(), 1);
+	let action = Type::Enum("ACTION".to_string(), action_values);
+
+	let hs_args: Vec<Type> = vec![
+		entry.clone(),
+		action,
+		Type::Pointer(Box::new(Type::Pointer(Box::new(entry)))),
+		Type::Pointer(Box::new(hs_data))
+	];
+	let hsrch = Function { return_type: Type::Integer, arguments: hs_args,
+	                       name: "hsearch_r".to_string() };
+	// "argument 1 of hcreate_r must be argument 3 of hsearch_r"
+	let tbl = DependentVariable {
+		name: "tbl".to_string(),
+		src: VariableSource::Parameter(&hcreate_r, 1),
+		dest: VariableUse::Argument(&hsrch, 3)
+	};
+	let srch_entry = FreeVariable {
+		name: "item".to_string(),
+		tested: ValueU64::new(),
+		dest: VariableUse::Argument(&hsrch, 0),
+		ty: &hsrch.arguments[0],
+	};
+	let srch_action = FreeVariable {
+		name: "action".to_string(),
+		tested: ValueU64::new(),
+		dest: VariableUse::Argument(&hsrch, 1),
+		ty: &hsrch.arguments[1]
+	};
+	// return type, but it actually comes from an argument...
+	//let arg_rtype = Type::Pointer(Box::new(Type::Pointer(Box::new(entry))));
+	let rv = FreeVariable {
+		name: "retval".to_string(),
+		tested: ValueU64::new(),
+		dest: VariableUse::Nil,
+		ty: &hsrch.arguments[2],
+	};
+
+
+
+
 	let fname: &'static str = "/tmp/fuzzapi.c";
 	let mut f = match File::create(fname) {
 		Err(e) => {
@@ -150,7 +257,7 @@ fn main() {
 		Ok(x) => x,
 	};
 	let mut used = ValueU64::new();
-	generate(&mut f, &vec![hcreate_r], &mut used);
+	generate(&mut f, &vec![&hcreate_r], &mut used);
 	let outname: &'static str = ".fuzziter";
 	let compile = match Command::new("gcc").arg("-Wall").arg("-Wextra")
 	                                       .arg("-fcheck-pointer-bounds")
