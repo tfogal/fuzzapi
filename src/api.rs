0253ebe06d953f5309ba33d5eb6f942729a8a133
Tom Fogal <tfogal@sci.utah.edu>
2017-03-09 21:25:13 -0800

    Rename "UDT" to "Struct".
    Enums are also user-defined types.

--- src/api.rs
+++ src/api.rs
@@ -12,14 +12,14 @@ pub type EnumValue = (String, i64);
 #[derive(Debug)]
 pub enum DeclType {
 	Builtin(Native),
-	UDT(Vec<Declaration>),
+	Struct(Vec<UDTDecl>),
 	Enum(Vec<EnumValue>),
-	UDTRef(String),
+	StructRef(String),
 	EnumRef(String),
 }
 
 #[derive(Debug)]
-pub struct Declaration {
+pub struct UDTDecl {
 	pub name: String,
 	pub ty: DeclType,
 }
@@ -34,14 +34,14 @@ mod test {
 		let s = "struct entry { }";
 		assert!(fuzz::parse_L_API(s).is_ok());
 		assert_eq!(fuzz::parse_L_API(s).unwrap().len(), 1);
-		let ref decl: api::Declaration = fuzz::parse_L_API(s).unwrap()[0];
+		let ref decl: api::UDTDecl = fuzz::parse_L_API(s).unwrap()[0];
 		assert_eq!(decl.name, "entry".to_string());
 		match decl.ty {
 			api::DeclType::Builtin(_) => panic!("type should be UDT, is Builtin"),
 			api::DeclType::Enum(_) => panic!("type should be UDT, is Enum"),
 			api::DeclType::EnumRef(_) => panic!("type should be UDT, is EnumRef"),
-			api::DeclType::UDTRef(_) => panic!("type should be UDT, is UDTRef"),
-			api::DeclType::UDT(ref decllist) => {
+			api::DeclType::StructRef(_) => panic!("type should be UDT, is StructRef"),
+			api::DeclType::Struct(ref decllist) => {
 				assert_eq!(decllist.len(), 0)
 			},
 		};
@@ -52,22 +52,22 @@ mod test {
 		let s = "struct Ent { pointer char key; }";
 		assert!(fuzz::parse_L_API(s).is_ok());
 		assert_eq!(fuzz::parse_L_API(s).unwrap().len(), 1);
-		let ref decl: api::Declaration = fuzz::parse_L_API(s).unwrap()[0];
+		let ref decl: api::UDTDecl = fuzz::parse_L_API(s).unwrap()[0];
 		assert_eq!(decl.name, "Ent".to_string());
 		match decl.ty {
 			api::DeclType::Builtin(_) => panic!("type should be UDT, is Builtin"),
 			api::DeclType::Enum(_) => panic!("type should be UDT, is Enum"),
 			api::DeclType::EnumRef(_) => panic!("type should be UDT, is EnumRef"),
-			api::DeclType::UDTRef(_) => panic!("type should be UDT, is UDTRef"),
-			api::DeclType::UDT(ref decllist) => {
+			api::DeclType::StructRef(_) => panic!("type should be UDT, is StructRef"),
+			api::DeclType::Struct(ref decllist) => {
 				assert_eq!(decllist.len(), 1);
-				let ref key: api::Declaration = decllist[0];
+				let ref key: api::UDTDecl = decllist[0];
 				assert_eq!(key.name, "key");
 				match key.ty {
-					api::DeclType::UDT(_) => panic!("incorrect type UDT for 'key'"),
+					api::DeclType::Struct(_) => panic!("incorrect type UDT for 'key'"),
 					api::DeclType::Enum(_) => panic!("incorrect type Enum for 'key'"),
 					api::DeclType::EnumRef(_) => panic!("incorrect type for 'key'"),
-					api::DeclType::UDTRef(_) => panic!("incorrect type for 'key'"),
+					api::DeclType::StructRef(_) => panic!("incorrect type for 'key'"),
 					api::DeclType::Builtin(ref blt) => {
 						let ch = api::Native::Character;
 						assert_eq!(blt, &api::Native::Pointer(Box::new(ch)));
@@ -85,34 +85,34 @@ mod test {
 		"}";
 		assert!(fuzz::parse_L_API(s.as_str()).is_ok());
 		assert_eq!(fuzz::parse_L_API(s.as_str()).unwrap().len(), 1);
-		let ref decl: api::Declaration = fuzz::parse_L_API(s.as_str()).unwrap()[0];
+		let ref decl: api::UDTDecl = fuzz::parse_L_API(s.as_str()).unwrap()[0];
 		assert_eq!(decl.name, "Entry".to_string());
 		match decl.ty {
 			api::DeclType::Builtin(_) => panic!("type should be UDT, is Builtin"),
 			api::DeclType::Enum(_) => panic!("type should be UDT, is Enum"),
 			api::DeclType::EnumRef(_) => panic!("type should be UDT, is EnumRef"),
-			api::DeclType::UDTRef(_) => panic!("type should be UDT, is UDTRef"),
-			api::DeclType::UDT(ref decllist) => {
+			api::DeclType::StructRef(_) => panic!("type should be UDT, is StructRef"),
+			api::DeclType::Struct(ref decllist) => {
 				assert_eq!(decllist.len(), 2);
-				let ref key: api::Declaration = decllist[0];
+				let ref key: api::UDTDecl = decllist[0];
 				assert_eq!(key.name, "key");
 				match key.ty {
-					api::DeclType::UDT(_) => panic!("incorrect type UDT for 'key'"),
+					api::DeclType::Struct(_) => panic!("incorrect type UDT for 'key'"),
 					api::DeclType::Enum(_) => panic!("incorrect type Enum for 'key'"),
 					api::DeclType::EnumRef(_) => panic!("incorrect type for 'key'"),
-					api::DeclType::UDTRef(_) => panic!("incorrect type for 'key'"),
+					api::DeclType::StructRef(_) => panic!("incorrect type for 'key'"),
 					api::DeclType::Builtin(ref blt) => {
 						let ch = api::Native::Character;
 						assert_eq!(blt, &api::Native::Pointer(Box::new(ch)));
 					}
 				}
-				let ref value: api::Declaration = decllist[1];
+				let ref value: api::UDTDecl = decllist[1];
 				assert_eq!(value.name, "value");
 				match value.ty {
-					api::DeclType::UDT(_) => panic!("incorrect type UDT for 'key'"),
+					api::DeclType::Struct(_) => panic!("incorrect type UDT for 'key'"),
 					api::DeclType::Enum(_) => panic!("incorrect type Enum for 'key'"),
 					api::DeclType::EnumRef(_) => panic!("incorrect type for 'key'"),
-					api::DeclType::UDTRef(_) => panic!("incorrect type for 'key'"),
+					api::DeclType::StructRef(_) => panic!("incorrect type for 'key'"),
 					api::DeclType::Builtin(ref blt) => {
 						let ch = api::Native::Void;
 						assert_eq!(blt, &api::Native::Pointer(Box::new(ch)));
