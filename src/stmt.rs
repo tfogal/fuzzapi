11390dbdb768c8b191bb9950908f9fc5f86794a8
Tom Fogal <tfogal@sci.utah.edu>
2017-04-18 22:04:15 -0700

    start code for representing statements and expressions.

---
+++ src/stmt.rs
@@ -0,0 +1,65 @@
+use function::*;
+use typ::*;
+use usergen::Opcode;
+use variable;
+
+// Code is anything we can generate code for.
+trait Code {
+	fn codegen(&self) -> String;
+}
+
+pub enum Expression {
+	Simple(variable::ScalarOp, variable::Source),
+	Compound(Box<Expression>, Opcode, Box<Expression>),
+	FqnCall(Function),
+}
+
+impl Expression {
+	pub fn exprtype(&self) -> Type {
+		match self {
+			&Expression::Simple(ref op, ref src) => {
+				let tbasic = src.root();
+				match op {
+					&variable::ScalarOp::Null => tbasic.ty,
+					&variable::ScalarOp::Deref => tbasic.ty.dereference(),
+					&variable::ScalarOp::AddressOf => Type::Pointer(Box::new(tbasic.ty)),
+				}
+			}
+			_ => unimplemented!(),
+		}
+	}
+}
+
+macro_rules! tryp {
+	($e:expr) => (match $e { Ok(f) => f, Err(g) => panic!("{}", g) })
+}
+
+impl Code for Expression {
+	fn codegen(&self) -> String {
+		use std::fmt::Write;
+		let mut rv = String::new();
+		match self {
+			&Expression::Simple(ref op, ref src) => {
+				if src.is_free() {
+					tryp!(write!(rv, "{}{}", op.to_string(), src.name()));
+				} else if src.is_bound() {
+					// correct?
+					tryp!(write!(rv, "{}{}", op.to_string(), src.root().name()));
+				} else if src.is_retval() {
+					tryp!(write!(rv, "/*fixme, from ret!*/"));
+				} else {
+					unreachable!();
+				}
+				rv
+			},
+			_ => unimplemented!(),
+		}
+	}
+}
+
+pub enum Statement {
+	Expr(Expression),
+	Assignment(Expression /* LHS */, Expression /* RHS */),
+	Verify(Expression),
+	/* should have 'if' and 'loop' etc. */
+}
